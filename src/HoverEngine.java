83065bcf82cf7ade580012a255c72bd2968b8d1f
MathLuke <lukemathieu06@gmail.com>
2023-02-21 19:51:04 -0500

    Updated HoverEngine.java

--- src/HoverEngine.java
+++ src/HoverEngine.java
@@ -14,6 +14,8 @@
 public class HoverEngine {
     /** The amount of energy stored within the engine. */
     private double energyAmount;
+    /** The maximum amount of energy the engine can store. */
+    private final double MAX_ENERGY;
     /** Whether the engine is on or not. */
     private boolean isEngineOn;
     /** The engine size (small, medium, large, etc.) */
@@ -21,26 +23,49 @@ public class HoverEngine {
     /** The maximum weight capacity the engine can support */
     private final double MAX_WEIGHT;
 
+    /**
+     * Constructor creates a HoverEngine object with default properties. Note that the default HoverEngine
+     * is not capable of supporting any additional weight.
+     */
     HoverEngine () {
         energyAmount = 0.0;
+        MAX_ENERGY = 0.0;
         isEngineOn = false;
         size = "Small";
         MAX_WEIGHT = 0.0;
     }
 
-    HoverEngine (double energyAmount, boolean isEngineOn, String size, double maxWeight) {
+    /**
+     * Constructor to create a HoverEngine with specific energy amount, engine status,
+     * engine size, and maximum weight.
+     * @param energyAmount The energy amount of the engine. If this amount is greater than the maximum energy capacity, will be set to the maximum capacity.
+     * @param maxEnergy The maximum energy capacity of the engine.
+     * @param isEngineOn Whether the engine is on or off.
+     * @param size The size of the engine (Small, Medium, Large, etc.)
+     * @param maxWeight The maximum weight that the engine can support.
+     */
+    HoverEngine (double energyAmount, double maxEnergy, boolean isEngineOn, String size, double maxWeight) {
         this.energyAmount = energyAmount;
+        MAX_ENERGY = maxEnergy;
+        if (this.energyAmount > MAX_ENERGY) {
+            this.energyAmount = MAX_ENERGY;
+        }
         this.isEngineOn = isEngineOn;
         this.size = size;
-        this.MAX_WEIGHT = maxWeight;
+        MAX_WEIGHT = maxWeight;
     }
 
     /**
-     * Adds an amount of charge to the engine.
+     * Adds an amount of charge to the engine. If charge to be added would exceed
+     * engine maximum energy capacity, engine energy level is set to the maximum.
      * @param amount The amount of charge to be added.
      */
     public void chargeEngine (double amount) {
-        energyAmount += amount;
+        if (energyAmount + amount > MAX_ENERGY) {
+            energyAmount = MAX_ENERGY;
+        } else {
+            energyAmount += amount;
+        }
     }
 
     /**
@@ -68,11 +93,12 @@ public class HoverEngine {
 
     /**
      * Decrements energyAmount by a specified amount, used whenever the engine is in operation.
+     *
      * @param amount The amount of energy to be decremented.
-     * @return True if energyAmount was successfully decremented, false if the amount is greater than the energyAmount
+     * @return True if energyAmount was successfully decremented, false if the amount is greater than the energyAmount or if the engine is not enabled.
      */
     public boolean useEngine(double amount) {
-        if (energyAmount >= amount) {
+        if (isEngineOn && energyAmount >= amount) {
             energyAmount -= amount;
             return true;
         } else {
@@ -110,5 +136,13 @@ public class HoverEngine {
      */
     double getMaxWeight () {
         return MAX_WEIGHT;
+    }
+
+    /**
+     * Getter for MAX_ENERGY variable.
+     * @return State of MAX_ENERGY.
+     */
+    double getMaxEnergy () {
+        return MAX_ENERGY;
     }
 }
